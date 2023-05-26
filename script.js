a5cc1801c28432ea665fee7b3e0ebfbc185fee92
Gopal Patel <130164161+Gopalptl1405@users.noreply.github.com>
2023-05-26 20:25:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 //your JS code here. If required.
-const sizeElement = document.createElement("h1");
-sizeElement.innerText = `Width: ${window.innerwidth} and Height: ${window.innerheight}`;
-document.body.append(sizeElement);
+// const sizeElement = document.createElement("h1");
+// sizeElement.innerText = `Width: ${window.innerwidth} and Height: ${window.innerheight}`;
+// document.body.append(sizeElement);
 
-window.onresize = function () {
-	sizeElement.innerText = `Width: ${window.innerwidth} and Height: ${window.innerheight}`;
+// window.onresize = function () {
+// 	sizeElement.innerText = `Width: ${window.innerwidth} and Height: ${window.innerheight}`;
 	
+// }
+let div = document.createElement("div");
+div.className= "sizeInfo";
+let h1 = document.createElement("h1");
+h1.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+document.body.append(div);
+div.append(h1);
+
+window.onresize = function(){
+ h1.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
 }
