4b756b86a94c9917df2876f36b045e5397f1a740
Gopal Patel <130164161+Gopalptl1405@users.noreply.github.com>
2023-05-26 19:45:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 //your JS code here. If required.
 const sizeElement = document.createElement("h1");
-sizeElement.innerText = `Width: $(window.width) and Height: $(window.height)`;
+sizeElement.innerText = `Width: ${window.innerwidth} and Height: ${window.innerheight}`;
+document.body.append(sizeElement);
